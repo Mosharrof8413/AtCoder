b9b0895a79375383175b6736099d8310b9249787
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2021-09-01 07:04:25 +0600

    Add files via upload

---
+++ C/C - One More aab aba baa .cpp
@@ -0,0 +1,98 @@
+/*
+ Problem Link : https://atcoder.jp/contests/abc215/tasks/abc215_c
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+
+
+ll gcd ( ll a, ll b )
+{
+    return __gcd ( a, b );
+}
+ll lcm ( ll a, ll b )
+{
+    return a * ( b / gcd ( a, b ) );
+}
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+const int MX=1e7+123;
+bitset<MX>is_prime;
+vector<ll>primes;
+void primeGen(ll n)
+{
+    ll sq=sqrt(n);
+    for(ll i=3; i<=n; i+=2)is_prime[i]=1;
+
+    for(ll i=3; i<=sq; i+=2)
+    {
+        if(is_prime[i]==1)
+        {
+            for(ll j=i*i; j<=n; j+=(i+i))
+                is_prime[j]=0;
+        }
+    }
+    primes.PB(2);
+    is_prime[2]=1;
+    for(ll i=3; i<=n; i+=2)
+    {
+        if(is_prime[i]==1)primes.PB(i);
+    }
+}
+ll NOD(ll n)
+{
+    ll ans=1;
+    for(auto u : primes)
+    {
+
+        if(u*u > n)break;
+        if(n%u==0)
+        {
+            ll cnt=0;
+            while(n%u==0)
+            {
+                n/=u;
+                cnt++;
+            }
+            cnt++;
+            ans*=cnt;
+        }
+    }
+    if(n>1) ans*=2;
+    return ans;
+}
+vector<ll>v;
+int main()
+{
+    optimize();
+   // ll limt=1e6+123;
+    //primeGen(limt);
+    ll test=1;
+    //cin>>test;
+    while(test--)
+    {
+       string st;
+       cin>>st;
+       ll k;
+       cin>>k;
+       vector<string>v;
+       sort(st.begin(),st.end());
+       v.PB(st);
+       while(next_permutation(st.begin(),st.end()))v.PB(st);
+       cout<<v[k-1]<<endl;
+    }
+}
+
+//Alhamdulillah..
+
+
+
